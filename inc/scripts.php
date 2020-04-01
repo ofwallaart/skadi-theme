343fdc9a11400e3e57b05c35c521261563f31e1d
Olaf <olafwallaart@gmail.com>
2020-04-01 15:13:18 +0200

    make scss work

--- inc/scripts.php
+++ inc/scripts.php
@@ -6,7 +6,7 @@ function materialwp_scripts() {
 
 	wp_enqueue_style( 'materialwp-material-icons', '//fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons', array(), '0.0.5' );
 
-	wp_enqueue_style( 'materialwp-style', get_stylesheet_directory_uri() . '/css/style.css', array(), '0.0.5' );
+	wp_enqueue_style( 'materialwp-style', get_stylesheet_directory_uri() . '/style.min.css', array(), '0.0.5' );
 
 	wp_enqueue_script( 'materialwp-js', get_template_directory_uri() . '/js/dist/scripts.min.js', array('jquery'), ' ', true );
 
