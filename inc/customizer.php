0ef492a33dbf0b3af37c89b589cd971689d488ea
Olaf Wallaart <olafwallaart@gmail.com>
2020-04-28 17:43:29 +0200

    fix lander image customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -151,7 +151,7 @@ function skadi_front_callout($wp_customize){
     ))));
 
 	$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize,
-	'skadi-front-image', array(
+	'skadi-thumbnail', array(
 		'label'=>'Thumbnail Image (appears in top-left corner)',
 		'section'=>'skadi-footer-image',
 		'settings'=> 'skadi-thumbnail-setting',
